e84346b08f07580e5c97466ae2a060a1809b8f87
Vladimir Shcheglov <vladimir@object.net>
2017-06-19 21:24:07 +0300

    [2769] System.Uri class is not reflectable (#2836)
    * [2769] System.Uri class is not reflectable
    
    * Update  Integration Test reference

--- Bridge/System/Uri.cs
+++ Bridge/System/Uri.cs
@@ -6,6 +6,7 @@ namespace System
     /// Provides an object representation of a uniform resource identifier (URI) and easy access to the parts of the URI.
     /// </summary>
     [External]
+    [Reflectable]
     public class Uri
     {
         public extern Uri(string uriString);
