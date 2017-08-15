d3aa52c2295f71e3e1617773788aa85c9196a43f
Leonid Veriga <leonid@object.net>
2017-08-15 20:51:11 +0300

    [V16.1] Correct file name casing for Integration Test reference files (#3059)
    * Clean Up Integration Test folders
    
    * Restore files with 100% correct file name case [skip ci] ci skip
    
    * Restore Integration Test reference files (correct file name casing only)
    
    * Include ignored files

--- Compiler/TranslatorTests/TestProjects/11/Bridge/reference/testProject2.js
+++
@@ -1,9 +0,0 @@
-Bridge.assembly("TestProject", function ($asm, globals) {
-    "use strict";
-
-    Bridge.define("TestProject2.TestClassB", {
-        props: {
-            Value1: 0
-        }
-    });
-});
