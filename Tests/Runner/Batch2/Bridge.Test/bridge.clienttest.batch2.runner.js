c77339eaead5b71db4e3943f61617450e0e126c5
Fabrício Zimmerer Murta <fabricio@object.net>
2017-12-08 04:11:55 -0200

    Updates bridge version number in output files.

--- Tests/Runner/Batch2/Bridge.Test/bridge.clienttest.batch2.runner.js
+++ Tests/Runner/Batch2/Bridge.Test/bridge.clienttest.batch2.runner.js
@@ -1,5 +1,5 @@
 /**
- * @compiler Bridge.NET 16.5.0
+ * @compiler Bridge.NET 16.5.1
  */
 Bridge.assembly("Bridge.Test.Bridge.ClientTest.Batch2", function ($asm, globals) {
     
--- Tests/Runner/Batch4/Bridge.Test/bridge.clienttest.batch4.runner.js
+++ Tests/Runner/Batch4/Bridge.Test/bridge.clienttest.batch4.runner.js
@@ -1,5 +1,5 @@
 /**
- * @compiler Bridge.NET 16.5.0
+ * @compiler Bridge.NET 16.5.1
  */
 Bridge.assembly("Bridge.Test.Bridge.ClientTest.Batch4", function ($asm, globals) {
     
