13cacc9d8087ba30f50ed69d4ced12ef5c61e180
Fabrício Zimmerer Murta <fabricio@object.net>
2017-12-21 11:01:16 -0200

    Updates Bridge Version to 16.6.0.

--- .build/common/CommonAssemblyInfo.cs
+++ .build/common/CommonAssemblyInfo.cs
@@ -1,9 +1,9 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("16.5.0.0")]
-[assembly: AssemblyFileVersion("16.5.1")]
+[assembly: AssemblyVersion("16.6.0.0")]
+[assembly: AssemblyFileVersion("16.6.0")]
 // Remember to update NuGet.Build.Package.targets with the same version value
-// <DefaultPackageVersion>16.5.1</DefaultPackageVersion>
-[assembly: AssemblyInformationalVersion("16.5.1")]
+// <DefaultPackageVersion>16.6.0</DefaultPackageVersion>
+[assembly: AssemblyInformationalVersion("16.6.0")]
 [assembly: AssemblyCompany("Object.NET, Inc.")]
 [assembly: AssemblyCopyright("Copyright 2008-2017 Object.NET, Inc.")]
