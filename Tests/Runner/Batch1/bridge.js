40b854f0a6c9f9ee19b3bdbcd4feec50e2fc0034
Fabrício Zimmerer Murta <fabricio@object.net>
2018-01-06 18:35:30 -0200

    Refreshes output files with recent Bridge changes.

--- Tests/Runner/Batch1/bridge.js
+++ Tests/Runner/Batch1/bridge.js
@@ -8699,8 +8699,12 @@ Bridge.Class.addExtend(System.Boolean, [System.IComparable$1(System.Boolean), Sy
                 d.kind = (d.kind !== undefined) ? d.kind : 0
 
                 if (d.ticks === undefined) {
-                    // js getTime() already returns UTC-based counter, so there's no timezone offset to apply.
-                    d.ticks = System.Int64(d.getTime()).mul(10000).add(System.DateTime.minOffset);
+                    if (d.kind === 1) {
+                        d.ticks = System.Int64(d.getTime()).mul(10000).add(System.DateTime.minOffset);
+                    }
+                    else {
+                        d.ticks = System.Int64(d.getTime() - d.getTimezoneOffset() * 60 * 1000).mul(10000).add(System.DateTime.minOffset);
+                    }
                 }
 
                 return d.ticks;
@@ -8759,15 +8763,20 @@ Bridge.Class.addExtend(System.Boolean, [System.IComparable$1(System.Boolean), Sy
                 millisecond = (millisecond !== undefined) ? millisecond : 0;
                 kind = (kind !== undefined) ? kind : 0;
 
-                var d = new Date(year, month - 1, day, hour, minute, second, millisecond);
+                var d,
+                    ticks;
+                
+                d = new Date(year, month - 1, day, hour, minute, second, millisecond);
                 d.setFullYear(year);
+
+                ticks = System.DateTime.getTicks(d);
 
                 if (kind === 1) {
                     d = new Date(d.getTime() - d.getTimezoneOffset() * 60 * 1000)
                 }
 
                 d.kind = kind;
-                d.ticks = System.DateTime.getTicks(d);
+                d.ticks = ticks;
                 
                 return d;
             },
@@ -9815,8 +9824,8 @@ Bridge.Class.addExtend(System.Boolean, [System.IComparable$1(System.Boolean), Sy
                     d1.setUTCMilliseconds(0);
                 }
 
-                d1.ticks = System.DateTime.getTicks(d1);
                 d1.kind = d.kind;
+                d1.ticks = System.DateTime.getTicks(d1);
 
                 return d1;
             },
