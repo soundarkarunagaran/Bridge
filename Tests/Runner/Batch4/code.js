6af808b252c93b70d156ca9808f5360ce502e146
Fabrício Zimmerer Murta <fabricio@object.net>
2018-01-24 01:36:58 -0200

    Refreshes output files with Bridge 16.7.1 version tag.

--- Tests/Runner/Batch4/code.js
+++ Tests/Runner/Batch4/code.js
@@ -1,9 +1,9 @@
 /**
  * Bridge Test library - general C# language tests for Portarelle
- * @version 16.7.0
+ * @version 16.7.1
  * @author Object.NET, Inc.
  * @copyright Copyright 2008-2018 Object.NET, Inc.
- * @compiler Bridge.NET 16.7.0
+ * @compiler Bridge.NET 16.7.1
  */
 Bridge.assembly("Bridge.ClientTest.Batch4", function ($asm, globals) {
     "use strict";
