b9acd15f52c3ffd0eef91ace6b2e5a25f777fee2
Fabrício Zimmerer Murta <fabricio@object.net>
2018-01-29 14:20:43 -0200

    Adds NUnit test for issue 3386.

---
+++ Compiler/TranslatorTests/TestProjects/16/BridgeIssues/3000-3999/N3386.cs
@@ -0,0 +1,20 @@
+﻿using Bridge;
+using Bridge.Html5;
+using System;
+using System.Collections.Generic;
+
+namespace Test.BridgeIssues.N3386
+{
+    /// <summary>
+    /// #3386 - the test here consists in checking whether a method with
+    /// default parameter values as float/double NaN can be built.
+    /// </summary>
+    public class N3386
+    {
+        public static void ConstantNaN(
+            float f = float.NaN,
+            double d = double.NaN)
+        {
+        }
+    }
+}
