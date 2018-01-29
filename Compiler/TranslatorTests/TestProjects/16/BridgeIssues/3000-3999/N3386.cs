e65140f18afd43cb21f86c7a863c4b7fa71f07e3
Fabrício Zimmerer Murta <fabricio@object.net>
2018-01-29 15:00:34 -0200

    Updates NUnit test adding more comments and expanding it.

--- Compiler/TranslatorTests/TestProjects/16/BridgeIssues/3000-3999/N3386.cs
+++ Compiler/TranslatorTests/TestProjects/16/BridgeIssues/3000-3999/N3386.cs
@@ -11,10 +11,27 @@ namespace Test.BridgeIssues.N3386
     /// </summary>
     public class N3386
     {
-        public static void ConstantNaN(
-            float f = float.NaN,
-            double d = double.NaN)
-        {
-        }
+        /// <summary>
+        /// Basic test according with the issue report.
+        /// </summary>
+        /// <param name="d">Double variable with default NaN.</param>
+        public static void ConstantNaN(double d = double.NaN) { }
+
+        /// <summary>
+        /// Additional constants that fell in the same scenario and needed
+        /// to be fixed, for double and float.
+        /// </summary>
+        /// <param name="fn">Float variable with default NaN.</param>
+        /// <param name="fpi">Float variable with default +∞.</param>
+        /// <param name="fni">Float variable with default -∞.</param>
+        /// <param name="dpi">Double variable with default +∞.</param>
+        /// <param name="dni">Double variable with default -∞.</param>
+        public static void ConstantCompTime(
+            float fn = float.NaN,
+            float fpi = float.PositiveInfinity,
+            float fni = float.NegativeInfinity,
+            double dpi = double.PositiveInfinity,
+            double dni = double.NegativeInfinity
+            ) { }
     }
 }
