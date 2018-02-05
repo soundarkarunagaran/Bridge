2bb4700fcd468d850c05409e14643333b9944c08
Fabrício Zimmerer Murta <fabricio@object.net>
2018-02-04 23:57:28 -0200

    Cleans up and adds descriptive comments to unit tests for issue 3385.

--- Tests/Batch3/BridgeIssues/3300/N3385.cs
+++ Tests/Batch3/BridgeIssues/3300/N3385.cs
@@ -1,19 +1,24 @@
-using Bridge.Html5;
 using Bridge.Test.NUnit;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Bridge.ClientTest.Batch3.BridgeIssues
 {
+    /// <summary>
+    /// The test here consists in checking whether a char converted into an
+    /// object can be cast back into char.
+    /// </summary>
     [TestFixture(TestNameFormat = "#3385 - {0}")]
     public class Bridge3385
     {
+        /// <summary>
+        /// Just instantiate an object with a 'char' constant then check
+        /// whether it can convert back instead of throwing an exception.
+        /// </summary>
         [Test]
         public static void TestObjectToChar()
         {
             object a = 'a';
-            Assert.AreEqual('a', Convert.ToChar(a));
+            Assert.AreEqual('a', Convert.ToChar(a), "Char encapsulated in an object can be cast back to a char.");
         }
     }
 }
