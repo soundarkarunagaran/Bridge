e59db98524ab742245df83e0b73f6d08e9f3e8b4
Fabrício Zimmerer Murta <fabricio@object.net>
2018-02-14 15:00:36 -0200

    Fixes client-side implementation of Convert.ToString().
    It was not considering when the object had an override for the ToString()
    method.
    
    Related github issue: 3415.

--- Bridge/Resources/Convert.js
+++ Bridge/Resources/Convert.js
@@ -271,6 +271,12 @@
                         return "";
                     }
 
+                    // If the object has an override to the toString() method,
+                    // then just return its result
+                    if (value.toString !== window.toString) {
+                        return value.toString();
+                    }
+
                     if (Bridge.isDate(value)) {
                         return System.DateTime.format(value, null, formatProvider || null);
                     }
