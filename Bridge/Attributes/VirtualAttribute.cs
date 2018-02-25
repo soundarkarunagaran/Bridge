0323fe33e45fab2338b8e3c173e7435b8294eae0
Geoffrey McGill <geoff@object.net>
2018-02-24 20:56:14 -0700

    Organize Bridge Attributes

---
+++ Bridge/Attributes/VirtualAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Bridge
+{
+    [External]
+    [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Interface | AttributeTargets.Delegate, AllowMultiple = false)]
+    [NonScriptable]
+    public sealed class VirtualAttribute : Attribute
+    {
+        public extern VirtualAttribute();
+        public extern VirtualAttribute(VirtualTarget target);
+    }
+
+    [External]
+    public enum VirtualTarget
+    {
+        All = 0,
+        Class = 1,
+        Interface = 2
+    }
+}
