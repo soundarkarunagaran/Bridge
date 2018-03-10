153324ca3487ba7c5928eb4f304582fcee59e6cf
Fabrício Zimmerer Murta <fabricio@object.net>
2018-03-10 00:16:50 -0300

    Refreshes test projects' output with updated Bridge files.

--- Tests/Runner/Batch1/bridge.js
+++ Tests/Runner/Batch1/bridge.js
@@ -29935,20 +29935,107 @@ Bridge.define("System.Text.RegularExpressions.RegexParser", {
     Bridge.define("System.ComponentModel.DefaultValueAttribute", {
         inherits: [System.Attribute],
         fields: {
-            value: null
+            _value: null
         },
         props: {
             Value: {
                 get: function () {
-                    return this.value;
+                    return this._value;
                 }
             }
         },
         ctors: {
+            $ctor11: function (type, value) {
+                this.$initialize();
+                System.Attribute.ctor.call(this);
+                // The try/catch here is because attributes should never throw exceptions.  We would fail to
+                // load an otherwise normal class.
+                try {
+                    if ((type.prototype instanceof System.Enum)) {
+                        this._value = System.Enum.parse(type, value, true);
+                    } else if (Bridge.referenceEquals(type, System.TimeSpan)) {
+                        throw System.NotImplemented.ByDesign;
+                        // TODO: NotSupported
+                        //_value = TimeSpan.Parse(value);
+                    } else {
+                        throw System.NotImplemented.ByDesign;
+                        // TODO: NotSupported
+                        //_value = Convert.ChangeType(value, type, CultureInfo.InvariantCulture);
+                    }
+                }
+                catch ($e1) {
+                    $e1 = System.Exception.create($e1);
+                }
+            },
+            $ctor2: function (value) {
+                this.$initialize();
+                System.Attribute.ctor.call(this);
+                this._value = Bridge.box(value, System.Char, String.fromCharCode, System.Char.getHashCode);
+            },
+            $ctor1: function (value) {
+                this.$initialize();
+                System.Attribute.ctor.call(this);
+                this._value = Bridge.box(value, System.Byte);
+            },
+            $ctor4: function (value) {
+                this.$initialize();
+                System.Attribute.ctor.call(this);
+                this._value = Bridge.box(value, System.Int16);
+            },
+            $ctor5: function (value) {
+                this.$initialize();
+                System.Attribute.ctor.call(this);
+                this._value = Bridge.box(value, System.Int32);
+            },
+            $ctor6: function (value) {
+                this.$initialize();
+                System.Attribute.ctor.call(this);
+                this._value = value;
+            },
+            $ctor9: function (value) {
+                this.$initialize();
+                System.Attribute.ctor.call(this);
+                this._value = Bridge.box(value, System.Single, System.Single.format, System.Single.getHashCode);
+            },
+            $ctor3: function (value) {
+                this.$initialize();
+                System.Attribute.ctor.call(this);
+                this._value = Bridge.box(value, System.Double, System.Double.format, System.Double.getHashCode);
+            },
             ctor: function (value) {
                 this.$initialize();
                 System.Attribute.ctor.call(this);
-                this.value = value;
+                this._value = Bridge.box(value, System.Boolean, System.Boolean.toString);
+            },
+            $ctor10: function (value) {
+                this.$initialize();
+                System.Attribute.ctor.call(this);
+                this._value = value;
+            },
+            $ctor7: function (value) {
+                this.$initialize();
+                System.Attribute.ctor.call(this);
+                this._value = value;
+            },
+            $ctor8: function (value) {
+                this.$initialize();
+                System.Attribute.ctor.call(this);
+                this._value = Bridge.box(value, System.SByte);
+            },
+            $ctor12: function (value) {
+                this.$initialize();
+                System.Attribute.ctor.call(this);
+                this._value = Bridge.box(value, System.UInt16);
+            },
+            $ctor13: function (value) {
+                this.$initialize();
+                System.Attribute.ctor.call(this);
+                this._value = Bridge.box(value, System.UInt32);
+            },
+            $ctor14: function (value) {
+                this.$initialize();
+                System.Attribute.ctor.call(this);
+                this._value = value;
             }
         },
         methods: {
@@ -29972,7 +30059,7 @@ Bridge.define("System.Text.RegularExpressions.RegexParser", {
                 return Bridge.getHashCode(this);
             },
             setValue: function (value) {
-                this.value = value;
+                this._value = value;
             }
         }
     });
