0c8a33876cab5db6dcf8b562ae7fdc94e3231dca
Fabrício Zimmerer Murta <fabricio@object.net>
2018-03-21 01:50:59 -0300

    Adds comments to unit tests related to ValueTuple support.

--- Tests/Batch1/CSharp7/TestDiscardsAndOutVars.cs
+++ Tests/Batch1/CSharp7/TestDiscardsAndOutVars.cs
@@ -3,10 +3,17 @@ using System;
 
 namespace Bridge.ClientTest.CSharp7
 {
+    /// <summary>
+    /// The tests here consists in ensuring the discard and out variables
+    /// works when translating code to Bridge.
+    /// </summary>
     [Category(Constants.MODULE_BASIC_CSHARP)]
     [TestFixture(TestNameFormat = "Discards and out variables - {0}")]
     public class TestDiscardsAndOutVars
     {
+        /// <summary>
+        /// A class implementing an out-variable-driven method.
+        /// </summary>
         class Point
         {
             public int X { get; }
@@ -18,12 +25,6 @@ namespace Bridge.ClientTest.CSharp7
                 Y = y;
             }
 
-            public void Deconstruct(out int x1, out int y1)
-            {
-                x1 = X;
-                y1 = Y;
-            }
-
             public void GetCoordinates(out int x, out int y)
             {
                 x = X;
@@ -31,22 +32,40 @@ namespace Bridge.ClientTest.CSharp7
             }
         }
 
+        /// <summary>
+        /// A method returning a tuple?
+        /// </summary>
+        /// <param name="itemId"></param>
+        /// <returns></returns>
         static (int price, int discount) GetPrice(int itemId)
         {
             var product = (500, 100);
             return product;
         }
 
+        /// <summary>
+        /// A method using an inline expression and returning to whatever is
+        /// passed, using the discards concept (underline keyword).
+        /// </summary>
         static void Do() => _ = 10 + 20;
 
-        static int Success()
+        /// <summary>
+        /// A simple method that just returns integer 1.
+        /// </summary>
+        /// <returns></returns>
+        static int Success(string message = "Success.")
         {
+            Assert.True(true, message);
             return 1;
         }
 
-        static int Fail()
+        /// <summary>
+        /// A simple method that will call Assert.Fail() and return int 2.
+        /// </summary>
+        /// <returns></returns>
+        static int Fail(string message = "Failed.")
         {
-            Assert.Fail();
+            Assert.Fail(message);
             return 2;
         }
 
@@ -54,36 +73,37 @@ namespace Bridge.ClientTest.CSharp7
         public static void TestBasic()
         {
             var r = int.TryParse("", out _);
-            Assert.False(r);
+            Assert.False(r, "Discards works for int.TryParse().");
 
             r = int.TryParse("", out var _);
-            Assert.False(r);
+            Assert.False(r, "Discards works for int.TryParse() when prefixed with 'var'.");
 
             r = int.TryParse("", out int _);
-            Assert.False(r);
+            Assert.False(r, "Discards works for int.TryParse() when prefixed with 'int' (type specified).");
 
             r = int.TryParse("1", out int ii);
-            Assert.True(r);
+            Assert.True(r, "int.TryParse runs successfully when a variable is defined within the method call body.");
+            Assert.AreEqual(1, ii, "int.TryParse() correctly fills the call-body-defined variable value when parsing was possible.");
 
             new Point(10, 20).GetCoordinates(out int px, out int py);
-            Assert.AreEqual(10, px);
-            Assert.AreEqual(20, py);
+            Assert.AreEqual(10, px, "Class method using call-body-defined out variables works (1/2).");
+            Assert.AreEqual(20, py, "Class method using call-body-defined out variables works (2/2).");
 
             (int price, _) = GetPrice(1);
-            Assert.AreEqual(500, price);
+            Assert.AreEqual(500, price, "Discards works for methods implementing ValueTuple return values.");
 
             var x = 21;
-            _ = x > 20 ? Success() : Fail();
+            _ = x > 20 ? Success("Discards in expressions works.") : Fail("There's an issue with discards in expressions evaluation.");
 
             Action a = () => {
-                var _ = x > 20 ? Success() : Fail();
+                var _ = x > 20 ? Success("Discards within method-scoped actions works.") : Fail("There's an issue with discards within action methods.");
             };
             a();
 
             object y = null;
             if (!(y is var _))
             {
-                Assert.Fail();
+                Assert.Fail("The 'is' test between an object variable and a discards reference failed to match the expected result.");
             }
         }
     }
--- Tests/Batch1/CSharp7/TestValueTupleAndDeconstruction.cs
+++ Tests/Batch1/CSharp7/TestValueTupleAndDeconstruction.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 
 namespace Bridge.ClientTest.CSharp7
 {
+    /// <summary>
+    /// The tests here consists in checking the ValueTuple component and also
+    /// exploring some Deconstruction usages.
+    /// </summary>
     [Category(Constants.MODULE_BASIC_CSHARP)]
     [TestFixture(TestNameFormat = "ValueTuple and deconstruction - {0}")]
     public class TestValueTupleAndDeconstruction
     {
+        /// <summary>
+        /// A simple class implementing deconstructor.
+        /// </summary>
         class Point
         {
             public int X { get; }
@@ -25,30 +32,38 @@ namespace Bridge.ClientTest.CSharp7
             }
         }
 
+        /// <summary>
+        /// A method returning a ValueTuple.
+        /// </summary>
+        /// <param name="itemId"></param>
+        /// <returns></returns>
         static (int price, int discount) GetPrice(int itemId)
         {
             var product = (500, 100);
             return product;
         }
 
+        /// <summary>
+        /// Test by exploring different ValueTuple and Deconstructor usages.
+        /// </summary>
         [Test]
         public static void TestBasic()
         {
             (var x1, var y1) = new Point(1, 2);
-            Assert.AreEqual(1, x1);
-            Assert.AreEqual(2, y1);
+            Assert.AreEqual(1, x1, "Point class' destructor returned the expected ValueTuple (1/2).");
+            Assert.AreEqual(2, y1, "Point class' destructor returned the expected ValueTuple (2/2).");
 
             var price = GetPrice(1);
-            Assert.AreEqual(500, price.price);
-            Assert.AreEqual(100, price.discount);
+            Assert.AreEqual(500, price.price, "Method returning a Value Tuple gives the expected tuple (1/2).");
+            Assert.AreEqual(100, price.discount, "Method returning a Value Tuple gives the expected tuple (2/2).");
 
             var (p, s) = GetPrice(1);
-            Assert.AreEqual(500, p);
-            Assert.AreEqual(100, s);
+            Assert.AreEqual(500, p, "Method returning a Value Tuple gives the expected broken down value (1/2).");
+            Assert.AreEqual(100, s, "Method returning a Value Tuple gives the expected broken down value (2/2).");
 
             (var p1, var s1) = GetPrice(1);
-            Assert.AreEqual(500, p1);
-            Assert.AreEqual(100, s1);
+            Assert.AreEqual(500, p1, "Method returning a Value Tuple gives the expected broken down, preinit value (1/2).");
+            Assert.AreEqual(100, s1, "Method returning a Value Tuple gives the expected broken down, preinit value (2/2).");
 
             var dic = new Dictionary<string, int> { ["Bob"] = 32, ["Alice"] = 17 };
 
@@ -56,15 +71,15 @@ namespace Bridge.ClientTest.CSharp7
             {
                 if (name == "Bob")
                 {
-                    Assert.AreEqual(32, age);
+                    Assert.AreEqual(32, age, "ValueTuple in dictionary traversal works (1/2).");
                 }
                 else if (name == "Alice")
                 {
-                    Assert.AreEqual(17, age);
+                    Assert.AreEqual(17, age, "ValueTuple in dictionary traversal works (2/2).");
                 }
                 else
                 {
-                    Assert.Fail();
+                    Assert.Fail("There's an issue traversing a Dictionary while assigning its values to a ValueTuple.");
                 }
             }
 
@@ -72,15 +87,15 @@ namespace Bridge.ClientTest.CSharp7
             {
                 if (name == "Bob")
                 {
-                    Assert.AreEqual(32, age);
+                    Assert.AreEqual(32, age, "ValueTuple in dictionary traversal (broken down) works (1/2).");
                 }
                 else if (name == "Alice")
                 {
-                    Assert.AreEqual(17, age);
+                    Assert.AreEqual(17, age, "ValueTuple in dictionary traversal (broken-down) works (2/2).");
                 }
                 else
                 {
-                    Assert.Fail();
+                    Assert.Fail("There's an issue traversing a Dictionary while assigning its values to a ValueTuple broken down, prefixed with 'var'.");
                 }
             }
         }
