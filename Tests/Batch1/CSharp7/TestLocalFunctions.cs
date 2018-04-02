cb54f8ae42bb8187fc6d854e96ef99a29d2978b9
Fabrício Zimmerer Murta <fabricio@object.net>
2018-04-02 18:05:52 -0300

    Expands on expression bodied local functions' tests.

--- Tests/Batch1/CSharp7/TestLocalFunctions.cs
+++ Tests/Batch1/CSharp7/TestLocalFunctions.cs
@@ -4,18 +4,17 @@ using System.Collections.Generic;
 namespace Bridge.ClientTest.CSharp7
 {
     /// <summary>
-    /// The test here consists in checking C# 7's 'is' syntax support.
+    /// The test here consists in checking C# 7's local functions syntax support.
     /// </summary>
     [Category(Constants.MODULE_BASIC_CSHARP)]
     [TestFixture(TestNameFormat = "Local function - {0}")]
     public class TestLocalFunctions
     {
         /// <summary>
-        /// Explores several 'is' pattern syntax variations, and fail if they
-        /// do not behave the expected way.
+        /// Explores several scenarios appliable to the local functions syntax.
         /// </summary>
         [Test(ExpectedCount = 4)]
-        public static void LocalFunctionTests()
+        public static void LocalFunctionsTests()
         {
             void DoNothing()
             {
@@ -49,8 +48,11 @@ namespace Bridge.ClientTest.CSharp7
             Assert.AreEqual(40, multiRes, "Local function with 'out' parameter works.");
         }
 
-        [Test]
-        public static void TestArrowLocalFunction()
+        /// <summary>
+        /// Tests expression-bodied local functions.
+        /// </summary>
+        [Test(ExpectedCount = 5)]
+        public static void ExpressioNBodiedLocalFunctionsTests()
         {
             var numbers = new List<string>() { "one", "two" };
 
@@ -58,7 +60,26 @@ namespace Bridge.ClientTest.CSharp7
 
             string Length() => $"length is {length}";
 
-            Assert.AreEqual("length is 2", Length());
+            Assert.AreEqual("length is 2", Length(), "Expression bodied local function returning a string works.");
+
+            void DoNothing() => Assert.True(true, "Expression bodied local functions can be called.");
+
+            int Add(int x, int y) => x + y;
+
+            LocalRetType AddCustomType(int x, int y) => new LocalRetType() { Value = x + y };
+
+            void Multiply(int x, int y, out int res) => res = x * y;
+
+            DoNothing();
+
+            Assert.AreEqual(40, Add(15, 25), "Expression bodied local function returning a value works.");
+
+            var customTypeResult = AddCustomType(1, 2);
+            Assert.AreEqual(3, customTypeResult.Value, "Expression bodied local function returning a custom type works.");
+
+            int multiRes;
+            Multiply(20, 2, out multiRes);
+            Assert.AreEqual(40, multiRes, "Expression bodied local function with 'out' parameter works.");
         }
 
         public class LocalRetType
