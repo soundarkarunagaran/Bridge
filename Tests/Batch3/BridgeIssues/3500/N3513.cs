5c63e3c08e92e9f4198108107e36e6c1100ee8c3
Fabrício Zimmerer Murta <fabricio@object.net>
2018-04-06 15:15:48 -0300

    Expands on unit tests for issue 3513.

--- Tests/Batch3/BridgeIssues/3500/N3513.cs
+++ Tests/Batch3/BridgeIssues/3500/N3513.cs
@@ -6,6 +6,10 @@ using System.Linq;
 
 namespace Bridge.ClientTest.Batch3.BridgeIssues
 {
+    /// <summary>
+    /// The test here consists in ensuring conversion from an extension method
+    /// to Action works in Bridge.
+    /// </summary>
     [TestFixture(TestNameFormat = "#3513 - {0}")]
     public class Bridge3513
     {
@@ -14,18 +18,30 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
             a();
         }
 
+        /// <summary>
+        /// Tests passing the string's extension method as a parameter to the
+        /// 'Action'-parametered function and checking whether the result is
+        /// the expected one.
+        /// </summary>
         [Test]
         public static void TestExtensionMethodAsAction()
         {
+            Bridge3513StringExtension.StaticMessage = "Inline extension conversion works.";
             Run("Hello, World!".Print);
+
+            Bridge3513StringExtension.StaticMessage = "Variable-bound extension conversion works.";
+            var str = "Hello, World!";
+            Run(str.Print);
         }
     }
 
-    public static class Bridge3513SomeEx
+    public static class Bridge3513StringExtension
     {
+        public static string StaticMessage = "passed";
+
         public static void Print(this string message)
         {
-            Assert.AreEqual("Hello, World!", message);
+            Assert.AreEqual("Hello, World!", message, StaticMessage);
         }
     }
 }
