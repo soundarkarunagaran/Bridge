f156881d5d94fa587c5f1385b0cfbef6d55b0ea5
Fabrício Zimmerer Murta <fabricio@object.net>
2018-06-12 00:36:24 -0300

    Adds comments to unit tests for issue 3625.

--- Tests/Batch3/BridgeIssues/3600/N3625.cs
+++ Tests/Batch3/BridgeIssues/3600/N3625.cs
@@ -6,9 +6,19 @@ using System.Linq;
 
 namespace Bridge.ClientTest.Batch3.BridgeIssues
 {
+    /// <summary>
+    /// The tests here consists in ensuring emission order of local functions
+    /// obey the order they are actually entered in code.
+    /// </summary>
     [TestFixture(TestNameFormat = "#3625 - {0}")]
     public class Bridge3625
     {
+        /// <summary>
+        /// Tests the order by declaring two local funcions, in such a way
+        /// that the second calls the first. If the local function is not
+        /// emitted in the correct order, the return value would not match
+        /// the expected one.
+        /// </summary>
         [Test]
         public static void TestLocalFns()
         {
@@ -22,8 +32,8 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
                 return "Two:" + One(msg);
             }
 
-            Assert.AreEqual("One", One("One"));
-            Assert.AreEqual("Two:One", Two("One"));
+            Assert.AreEqual("One", One("One"), "First local function call matches expected result.");
+            Assert.AreEqual("Two:One", Two("One"), "Second local function call matches expected result.");
         }
     }
 }
