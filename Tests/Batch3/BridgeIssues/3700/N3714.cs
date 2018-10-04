ff4f7aa789bdd2dba47c739128d61ce9949b6957
Fabrício Zimmerer Murta <fabricio@object.net>
2018-10-03 22:40:09 -0300

    Reviews unit tests for issue 3714.

--- Tests/Batch3/BridgeIssues/3700/N3714.cs
+++ Tests/Batch3/BridgeIssues/3700/N3714.cs
@@ -1,12 +1,26 @@
 using Bridge.Test.NUnit;
-using System.Collections;
-using System.Collections.Generic;
 
 namespace Bridge.ClientTest.Batch3.BridgeIssues
 {
     public class SubClass : BaseClass
     {
         public SubClass() : base(1)
+        {
+
+        }
+    }
+
+    public class SubClass0 : BaseClass
+    {
+        public SubClass0() : base(0)
+        {
+
+        }
+    }
+
+    public class SubClass0m : BaseClass
+    {
+        public SubClass0m() : base(0m)
         {
 
         }
@@ -31,7 +45,17 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
             var test = new SubClass();
             var v = test.Value + test.Value;
 
-            Assert.True(v == 2m);
+            Assert.True(v == 2m, "Implicit conversion on base constructor call works when passing 1 to decimal.");
+
+            var test0 = new SubClass0();
+            var v0 = test0.Value + test0.Value;
+
+            Assert.True(v0 == 0, "Implicit conversion on base constructor call works when passing 0 to decimal.");
+
+            var test0m = new SubClass0m();
+            var v0m = test0.Value + test0.Value;
+
+            Assert.True(v0m == 0, "Base constructor call works when passing 0m to decimal (explicit conversion).");
         }
     }
 }
