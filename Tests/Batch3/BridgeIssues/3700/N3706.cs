bcbb5e2fb1023d10fd25cb1473d9a617b4ae6c81
Fabrício Zimmerer Murta <fabricio@object.net>
2018-10-03 22:10:05 -0300

    Reviews unit test for issue 3706.

--- Tests/Batch3/BridgeIssues/3700/N3706.cs
+++ Tests/Batch3/BridgeIssues/3700/N3706.cs
@@ -1,6 +1,5 @@
 using Bridge.Test.NUnit;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 
 namespace Bridge.ClientTest.Batch3.BridgeIssues
@@ -8,28 +7,28 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
     [TestFixture(TestNameFormat = "#3706 - {0}")]
     public class Bridge3706
     {
-        public static void FooWorking(KeyValuePair<String, Action>[] actions)
+        public static void FooWorking(KeyValuePair<String, Action<string>>[] actions)
         {
-            actions[0].Value();
+            actions[0].Value("KeyValuePair[]");
         }
 
-        public static void FooFails(IList<KeyValuePair<String, Action>> actions)
+        public static void FooFails(IList<KeyValuePair<String, Action<string>>> actions)
         {
-            actions[0].Value();
+            actions[0].Value("IList-KeyValuePair");
         }
 
-        public static void Test()
+        public static void Test(string what)
         {
-            Assert.True(true, "Action is invoked");
+            Assert.True(true, what + " action is invoked");
         }
 
         [Test(ExpectedCount = 2)]
         public static void TestIListIndexer()
         {
-            var l = new KeyValuePair<String, Action>[]
-               {
-                    new KeyValuePair<String, Action>("Bar", Test),
-               };
+            var l = new KeyValuePair<String, Action<string>>[]
+            {
+            new KeyValuePair<String, Action<string>>("Bar", Test),
+            };
             FooWorking(l);
             FooFails(l);
         }
