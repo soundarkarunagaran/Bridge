6b42ef6cfadd70c960e9e16fcd785f872064aec7
Fabrício Zimmerer Murta <fabricio@object.net>
2018-10-03 22:08:04 -0300

    Adds NUnit test for issue 3702 typescript output.

---
+++ Compiler/TranslatorTests/TestProjects/11/Issues/N3702.cs
@@ -0,0 +1,17 @@
+﻿using Bridge;
+
+//#3702 - TypeScript output's [Optional] attribute.
+namespace Issue3702
+{
+    public class Person
+    {
+        /// <summary>
+        /// This should output as "Name?: string | null
+        /// On the .ts file output.
+        /// </summary>
+        [Optional]
+        public string Name { get; set; }
+
+        public string Company;
+    }
+}
