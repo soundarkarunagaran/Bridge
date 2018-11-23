3955fb28e5f21f5715e903095deaf5883baecbf5
Fabrício Zimmerer Murta <fabricio@object.net>
2018-11-23 20:45:55 -0200

    Comments up unit test for issue 3679.

--- Tests/Batch3/BridgeIssues/3600/N3679.cs
+++ Tests/Batch3/BridgeIssues/3600/N3679.cs
@@ -1,13 +1,20 @@
 using Bridge.Test.NUnit;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 
 namespace Bridge.ClientTest.Batch3.BridgeIssues
 {
+    /// <summary>
+    /// This consists of checking whether InvariantCultureIgnoreCase IndexOf()
+    /// works on strings.
+    /// </summary>
     [TestFixture(TestNameFormat = "#3679 - {0}")]
     public class Bridge3679
     {
+        /// <summary>
+        /// Simply part from a simple 'hello world' example to see whether
+        /// we can use the method from String class:
+        /// String.IndexOf(StringComparison.InvariantCultureIgnoreCase)
+        /// </summary>
         [Test]
         public static void TestStringIndexOf()
         {
@@ -15,7 +22,7 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
 
             int i = msg.IndexOf("world", 3, StringComparison.InvariantCultureIgnoreCase);
 
-            Assert.AreEqual(7, i);
+            Assert.AreEqual(7, i, "IndexOf, InvariantCulture, IgnoringCase: works.");
         }
     }
 }
