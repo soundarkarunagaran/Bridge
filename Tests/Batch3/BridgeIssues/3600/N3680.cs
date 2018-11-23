ca3ca0752829df5526aafc379bedf3dd60449bbe
Fabrício Zimmerer Murta <fabricio@object.net>
2018-11-23 21:20:56 -0200

    Comments up unit tests for issue 3680.

--- Tests/Batch3/BridgeIssues/3600/N3680.cs
+++ Tests/Batch3/BridgeIssues/3600/N3680.cs
@@ -5,6 +5,10 @@ using System.Collections.Generic;
 
 namespace Bridge.ClientTest.Batch3.BridgeIssues
 {
+    /// <summary>
+    /// This tests whether CreateInstance throws the expected method, yet does
+    /// not lose its functionality to normal cases.
+    /// </summary>
     [TestFixture(TestNameFormat = "#3680 - {0}")]
     public class Bridge3680
     {
@@ -21,16 +25,22 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
             string A { get; set; }
         }
 
+        /// <summary>
+        /// Explores the potential issues for class, interface, and also checks
+        /// some situations where it should work.
+        /// </summary>
         [Test]
         public static void TestActivator()
         {
-            Assert.Throws<MissingMethodException>(() => Activator.CreateInstance(typeof(Test)));
-            Assert.Throws<MissingMethodException>(() => Activator.CreateInstance(typeof(ITest)));
+            Assert.Throws<MissingMethodException>(() => Activator.CreateInstance(typeof(Test)),
+                "Expected exception thrown for class.");
+            Assert.Throws<MissingMethodException>(() => Activator.CreateInstance(typeof(ITest)),
+                "Expected exception thrown for interface.");
 
             var test = Activator.CreateInstance(typeof(Test), true);
 
-            Assert.NotNull(test);
-            Assert.True(test is Test);
+            Assert.NotNull(test, "CreateIntance() results in a non-null result.");
+            Assert.True(test is Test, "CreateInstance() results in an instance of the expected class.");
         }
     }
 }
