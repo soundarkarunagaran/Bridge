2a59f41380497c15f7701df8e1a30fa40192604f
Fabrício Zimmerer Murta <fabricio@object.net>
2018-11-23 19:43:02 -0200

    Comments up unit tests for issue 3467.

--- Tests/Batch3/BridgeIssues/3400/N3467.cs
+++ Tests/Batch3/BridgeIssues/3400/N3467.cs
@@ -1,11 +1,14 @@
 using Bridge.Test.NUnit;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using static Bridge.ClientTest.Batch3.BridgeIssues.Bridge3467_Test1;
 
 namespace Bridge.ClientTest.Batch3.BridgeIssues
 {
+    /// <summary>
+    /// This tests whether a sealed nested class set up works on Bridge.
+    /// The way the tests are written allows it to break the code no matter
+    /// how minification is set in bridge.json -- as long as the issue is still
+    /// present.
+    /// </summary>
     sealed class Bridge3467_Test1 : Bridge3467_ITest<NestedClass>
     {
         public sealed class NestedClass { }
@@ -17,10 +20,14 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
     [TestFixture(TestNameFormat = "#3467 - {0}")]
     public class Bridge3467
     {
+        /// <summary>
+        /// Just check if a new instance of the class is not null. Actually
+        /// what counts here is just if the code will result in runnable js.
+        /// </summary>
         [Test]
         public static void TestGenericUsingStatic()
         {
-            Assert.NotNull(new Bridge3467_Test1());
+            Assert.NotNull(new Bridge3467_Test1(), "Instantiating nested, sealed classes works.");
         }
     }
 }
