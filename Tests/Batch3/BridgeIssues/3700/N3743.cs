2c2273da1e177e3703b13f33d02116b414e2b326
Fabrício Zimmerer Murta <fabricio@object.net>
2018-11-24 02:29:27 -0200

    Comments up unit test for issue 3743.

--- Tests/Batch3/BridgeIssues/3700/N3743.cs
+++ Tests/Batch3/BridgeIssues/3700/N3743.cs
@@ -3,6 +3,9 @@ using System.Threading.Tasks;
 
 namespace Bridge.ClientTest.Batch3.BridgeIssues
 {
+    /// <summary>
+    /// Ensures /for loop/ variables within async methods are handled right.
+    /// </summary>
     [TestFixture(TestNameFormat = "#3743 - {0}")]
     public class Bridge3743
     {
@@ -17,13 +20,17 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
             return 1;
         }
 
+        /// <summary>
+        /// Just call the async function and check whether it returns from the
+        /// call, meaning the generated code didn't break on translation.
+        /// </summary>
         [Test]
         public async static void TestForLoopAsync()
         {
             var done = Assert.Async();
 
             var r = await TestAsync();
-            Assert.AreEqual(1, r);
+            Assert.AreEqual(1, r, "Async with 'for' with variables initialization works.");
 
             done();
         }
