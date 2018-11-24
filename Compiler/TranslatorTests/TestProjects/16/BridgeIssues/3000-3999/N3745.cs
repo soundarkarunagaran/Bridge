35224370e84d126b535fcf9931aa04e8615ff86f
Fabrício Zimmerer Murta <fabricio@object.net>
2018-11-23 22:14:25 -0200

    Adds NUnit integration test for issue 3745.

---
+++ Compiler/TranslatorTests/TestProjects/16/BridgeIssues/3000-3999/N3745.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Test.BridgeIssues.N3745
+{
+    /// <summary>
+    /// #3745 ensure an extra variable definition line is not output when using
+    /// implicit functions code (not confirmed) C#7 syntax.
+    /// </summary>
+    public class N3745
+    {
+        public static Action<Task<object>> f3(Func<Task<object>> f1)
+        {
+            //@ // p1 below shouldn't show in both function argument
+            //@ // and variables declaration.
+            return async p1 =>
+            {
+                p1 = null;
+
+                await f1();
+                return;
+            };
+        }
+    }
+}
