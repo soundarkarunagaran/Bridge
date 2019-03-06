cc77194d622f3afaa10e42d60cd6ad88b1cb34ba
Fabrício Zimmerer Murta <fabricio@object.net>
2019-03-06 10:26:59 -0300

    Adds descriptive comments to issue 3809 unit test.
    The issue requires quite a specific set up in order to be reproduced,
    and it could later be mistaken by a simpler scenario (where it would
    work even without the fix). This information was added to the unit tests
    to avoid simplifying it in the future, as Bridge.Html5 framework may at
    some point be discontinued.

--- Tests/Batch3/BridgeIssues/3800/N3809.cs
+++ Tests/Batch3/BridgeIssues/3800/N3809.cs
@@ -1,17 +1,28 @@
+// this is required in order to reproduce the issue (read below)
 using Bridge.Html5;
 using Bridge.Test.NUnit;
-using System;
-using System.Collections.Generic;
 
 namespace Bridge.ClientTest.Batch3.BridgeIssues
 {
+    /// <summary>
+    /// Ensures that external referenced and related types' result from
+    /// Type.IsAssignableFrom() works.
+    /// </summary>
+    /// <remarks>
+    /// Just trying to define the class hierarchy (HTMLElement/HTMLDivElement)
+    /// in a single-file class won't do to test against this scenario. This may
+    /// require the code to sit in a different assembly!
+    /// </remarks>
     [TestFixture(TestNameFormat = "#3809 - {0}")]
     public class Bridge3809
     {
         [Test]
         public static void TestIsAssignableFrom()
         {
-            Assert.True(typeof(HTMLElement).IsAssignableFrom(typeof(HTMLDivElement)));
+            // The checked types must come from a different assembly!
+            Assert.True(
+                typeof(HTMLElement).IsAssignableFrom(typeof(HTMLDivElement)),
+                "Type.IsAssignableFrom works with types from a different assembly.");
         }
     }
 }
