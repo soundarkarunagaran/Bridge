a75a4fe165eabf51de5725b03c754056ac01b994
Fabrício Zimmerer Murta <fabricio@object.net>
2019-03-06 08:47:27 -0300

    Expands and adds comments on unit tests for issues 3827, 3828.
    The actual 3827 part of the issue can be tested simply by having IE11
    load the tests, but not all tests are expected to run in IE11 anyway.

--- Tests/Batch3/BridgeIssues/3800/N3828.cs
+++ Tests/Batch3/BridgeIssues/3800/N3828.cs
@@ -5,16 +5,41 @@ using System.Linq;
 
 namespace Bridge.ClientTest.Batch3.BridgeIssues
 {
-    [TestFixture(TestNameFormat = "#3828 - {0}")]
+    
+    /// <summary>
+    /// The test here consists in ensuring SortedList usage is compatible
+    /// with Microsoft documentation on the class, and that the getter
+    /// method work. It was resulting in recursive stack loop in modern
+    /// browsers when used, and due to 'strict mode', Internet Explorer
+    /// 11 refused to load the bridge.js file, as its client-side getter
+    /// method was defined in duplicity.
+    /// </summary>
+    /// <remarks>
+    /// Two github issues were created: #3827 related to IE11 issue, and
+    /// #3828 related to the bracket-binding and exception thrown if getting
+    /// non-existing element.
+    /// </remarks>
+    [TestFixture(TestNameFormat = "#3827, #3828 - {0}")]
     public class Bridge3828
     {
+        /// <summary>
+        /// Tests basic usage of the Sorted list and ensures it complies with
+        /// the official documentation at
+        /// https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.sortedlist-2?view=netframework-4.7.2
+        /// </summary>
         [Test]
         public static void TestSortedList()
         {
             var list = new SortedList<int, string>();
             list[1] = "value1";
+            list.Add(2, "value2");
 
-            Assert.AreEqual(list[1], "value1");
+            Assert.AreEqual(2, list.Count, "SortedList has two elements.");
+            Assert.AreEqual(list[1], "value1", "SortedList's get method works for first element.");
+            Assert.AreEqual(list[2], "value2", "SortedList's get method works for second element.");
+            Assert.Throws<KeyNotFoundException>(() => {
+                var z = list[4];
+            }, "Fetching non-existing SortedList element throws System.KeyNotFoundException.");
         }
     }
 }
