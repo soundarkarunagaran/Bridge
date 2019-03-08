30ed656e6b77ce43996bba24d49683d4a2db72ca
Fabrício Zimmerer Murta <fabricio@object.net>
2019-03-08 00:58:27 -0300

    Cleanup and comments to issue 3865 unit test.

--- Tests/Batch3/BridgeIssues/3800/N3865.cs
+++ Tests/Batch3/BridgeIssues/3800/N3865.cs
@@ -1,17 +1,21 @@
 using Bridge.Test.NUnit;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 
 namespace Bridge.ClientTest.Batch3.BridgeIssues
 {
+    /// <summary>
+    /// The tests here consists in ensuring reflection works over an array of
+    /// arrays.
+    /// </summary>
     [TestFixture(TestNameFormat = "#3865 - {0}")]
     public class Bridge3865
     {
         [Test]
         public static void TestArrayAssembly()
         {
-            Assert.AreEqual(typeof(string).Assembly.FullName, typeof(string[][]).Assembly.FullName);
+            Assert.AreEqual(
+                typeof(string).Assembly.FullName,
+                typeof(string[][]).Assembly.FullName,
+                "The type associated to an array of arrays has the Assembly property.");
         }
     }
 }
