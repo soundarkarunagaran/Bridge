4f2a7fe3001528fd7563c27f0c4d8f4a72a01449
Fabrício Zimmerer Murta <fabricio@object.net>
2019-03-11 16:40:30 -0300

    Adds comments to issue 3867 unit tests.

--- Tests/Batch3/BridgeIssues/3800/N3867.cs
+++ Tests/Batch3/BridgeIssues/3800/N3867.cs
@@ -1,7 +1,4 @@
 using Bridge.Test.NUnit;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Reflection;
 
 namespace Bridge.ClientTest.Batch3.BridgeIssues
@@ -21,15 +18,15 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
             var c = new MyModel { Prop = 1 };
             var t = typeof(MyModel);
             var fields = t.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-            Assert.AreEqual(1, fields.Length);
+            Assert.AreEqual(1, fields.Length, "fields length is right: 1");
 
             var field = fields[0];
-            Assert.AreEqual("<Prop>k__BackingField", field.Name);
-            Assert.AreEqual(1, field.GetValue(c));
+            Assert.AreEqual("<Prop>k__BackingField", field.Name, "field.Name is '<Prop>k__BackingField'");
+            Assert.AreEqual(1, field.GetValue(c), "field.GetValue() fetches the correct value: 1");
 
             field.SetValue(c, 2);
-            Assert.AreEqual(2, field.GetValue(c));
-            Assert.AreEqual(2, c.Prop);
+            Assert.AreEqual(2, field.GetValue(c), "Afted field.SetValue(2), field.GetValue() returns the expected value: 2");
+            Assert.AreEqual(2, c.Prop, "After field.SetValue(2), the target variable's respective property is changed accordingly");
         }
     }
 }
