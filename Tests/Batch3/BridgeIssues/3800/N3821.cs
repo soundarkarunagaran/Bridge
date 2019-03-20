3f63c4cd37ef0775e437c6abda1ca752dd30165c
Fabrício Zimmerer Murta <fabricio@object.net>
2019-03-20 16:15:08 -0300

    Reviews unit test for issue 3821.

--- Tests/Batch3/BridgeIssues/3800/N3821.cs
+++ Tests/Batch3/BridgeIssues/3800/N3821.cs
@@ -1,10 +1,12 @@
 using Bridge.Test.NUnit;
 using System;
-using System.Reflection;
-using System.Threading;
 
 namespace Bridge.ClientTest.Batch3.BridgeIssues
 {
+    /// <summary>
+    /// The tests here consists in ensuring thrown exceptions by
+    /// ClientWebSocket's ConnectAsync() when something goes wrong.
+    /// </summary>
     [TestFixture(TestNameFormat = "#3821 - {0}")]
     public class Bridge3821
     {
@@ -16,12 +18,12 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
             {               
                 var sc = new System.Net.WebSockets.ClientWebSocket();
                 await sc.ConnectAsync(new Uri("wss://NotExistServer/NotExist"), System.Threading.CancellationToken.None);
-                Assert.Fail();
+                Assert.Fail("ConnectAsync() to an invalid address didn't throw an exception.");
                 done();
             }
             catch (Exception ex)
             {
-                Assert.NotNull(ex);
+                Assert.NotNull(ex, "ConnectAsync() thrown an exception when trying to connect to an invalid address.");
                 done();
             }
         }
