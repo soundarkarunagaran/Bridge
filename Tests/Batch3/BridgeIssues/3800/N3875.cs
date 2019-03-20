a5cf482d9bcee0de1b9c735d84c9e02faddc8d14
Fabrício Zimmerer Murta <fabricio@object.net>
2019-03-20 13:11:40 -0300

    Reviews and expands unit tests for issue 3875.

--- Tests/Batch3/BridgeIssues/3800/N3875.cs
+++ Tests/Batch3/BridgeIssues/3800/N3875.cs
@@ -1,6 +1,5 @@
 using Bridge.Test.NUnit;
 using System.Linq;
-using System.Reflection;
 
 namespace Bridge.ClientTest.Batch3.BridgeIssues
 {
@@ -11,8 +10,24 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
         public static void TestToArray()
         {
             var bytes = new byte[] { 1, 2, 3, 4 };
-            Assert.AreEqual("Byte[]", bytes.ToArray().GetType().Name);
-            Assert.AreEqual("Byte[]", bytes.GetType().Name);
+            Assert.AreEqual("Byte[]", bytes.ToArray().GetType().Name, "Byte array type from linq's ToArray() is 'Byte[]'.");
+            Assert.AreEqual("Byte[]", bytes.GetType().Name, "Byte array's type is 'Byte[]'.");
+
+            var ints = new int[] { 1, 2, 3, 4 };
+            Assert.AreEqual("Int32[]", ints.ToArray().GetType().Name, "Int array type from linq's ToArray() is 'Int32[]'.");
+            Assert.AreEqual("Int32[]", ints.GetType().Name, "Int array's type is 'Int32[]'.");
+
+            var strings = new string[] { "1", "2", "3", "4" };
+            Assert.AreEqual("String[]", strings.ToArray().GetType().Name, "String array type from linq's ToArray() is 'String[]'.");
+            Assert.AreEqual("String[]", strings.GetType().Name, "String array's type is 'String[]'.");
+
+            var floats = new float[] { 1, 2, 3, 4 };
+            Assert.AreEqual("Single[]", floats.ToArray().GetType().Name, "Float array type from linq's ToArray() is 'Single[]'.");
+            Assert.AreEqual("Single[]", floats.GetType().Name, "Single array's type is 'Single[]'.");
+
+            var doubles = new double[] { 1.8, 2.2, 3.6, 4.1 };
+            Assert.AreEqual("Double[]", doubles.ToArray().GetType().Name, "Double array type from linq's ToArray() is 'Double[]'.");
+            Assert.AreEqual("Double[]", doubles.GetType().Name, "Double array's type is 'Double[]'.");
         }
     }
 }
