97ccda126b8b5f9ab8d6fe2547a30895365b25ca
Fabrício Zimmerer Murta <fabricio@object.net>
2019-05-14 10:08:59 -0300

    Adds descriptive comments to issue 3903 unit tests.

--- Tests/Batch3/BridgeIssues/3900/N3903.cs
+++ Tests/Batch3/BridgeIssues/3900/N3903.cs
@@ -1,11 +1,18 @@
 using Bridge.Test.NUnit;
-using System.Reflection;
 
 namespace Bridge.ClientTest.Batch3.BridgeIssues
 {
+    /// <summary>
+    /// The tests here consists in ensuring the XOR result involving boolean
+    /// values is sane and matches native .NET's.
+    /// </summary>
     [TestFixture(TestNameFormat = "#3903 - {0}")]
     public class Bridge3903
     {
+        /// <summary>
+        /// Simply define some boolean values and subject them to XOR tests
+        /// with the "^" (circumflex) operator.
+        /// </summary>
         [Test]
         public static void TestBooleanXor()
         {
@@ -15,8 +22,8 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
             var b = t;
             b ^= t;
 
-            Assert.True(a == true);
-            Assert.True(b == false);
+            Assert.True(a == true, "true ^ false evaluates to true.");
+            Assert.True(b == false, "true ^= true gets assigned to false.");
         }
     }
 }
