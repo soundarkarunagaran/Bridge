b1e34bc2cf52c7998e9e815da33c06d47e3e16ec
Fabrício Zimmerer Murta <fabricio@object.net>
2019-05-26 15:36:49 -0300

    Ignores test for issue #1509 if the browser is Firefox.
    Firefox 60+ only returns an integer from performance.now().

--- Tests/Batch3/BridgeIssues/1500/N1509.cs
+++ Tests/Batch3/BridgeIssues/1500/N1509.cs
@@ -15,12 +15,33 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
                 p = Bridge.Html5.Global.Performance.Now();
                 if (!HasNoFraction(p))
                 {
-                    Assert.True(true, "Did " + i + " attempt(s) to check performance.now() returns float");
+                    Assert.True(true, "Returned float from performance.now() in " + i + "/1000 attempts.");
                     return;
                 }
             }
 
-            Assert.Fail("performance.now() did 1000 attemps to check if it returns float");
+            /*
+             * Firefox 60+ would never return a float from performance.now.
+             * According to
+             * https://developer.mozilla.org/en-US/docs/Web/API/Performance/now:
+             *   "The timestamp is not actually high-resolution. To mitigate
+             *    security threats such as Spectre, browsers currently round
+             *    the results to varying degrees.
+             * >>>(Firefox started rounding to 1 millisecond in Firefox 60.)
+             *    Some browsers may also slightly randomize the timestamp. The
+             *    precision may improve again in future releases; browser
+             *    developers are still investigating these timing attacks and
+             *    how best to mitigate them."
+             */
+            if (Browser.FirefoxVersion >= 60)
+            {
+                Assert.True(true, "Firefox 60+ has performance.now() return exact 1ms-step values.");
+                return;
+            }
+            else
+            {
+                Assert.Fail("Not returned float from performance.now() in 1000 attemps.");
+            }
         }
 
         private bool HasNoFraction(double n)
@@ -29,3 +50,5 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
         }
     }
 }
+ 
+ 
